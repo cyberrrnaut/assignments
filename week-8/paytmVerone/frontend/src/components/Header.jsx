601facd9661df9ff15c3916c4c98f20ff3cad468
cybernaut <cyberrrnaut@icloud.com>
2024-06-30 19:55:56 +0530

    no more lafanga

--- week-8/paytmVerone/frontend/src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-
-export const Header = ({label})=>{
-   return <div className="font-bold text-4xl pt-6">
-    {label}
-   </div>
-}
--- week-8/paytmVerone/frontend/src/components/SubHeader.jsx
+++
@@ -1,9 +0,0 @@
-
-export const SubHeader = ({label})=>{
-
-    return <div className=" text-md text-slate-500 pt-1 px-4 pb-4">
-
-        {label}
-    </div>
-    
-}
